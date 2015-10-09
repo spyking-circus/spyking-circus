43dcc629dfefc199d48f9d53a24bad31f574cff2
Marcel Stimberg <marcel.stimberg@inserm.fr>
2015-10-09 14:47:24 +0200

    Rename the package to spyking-circus

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import os
 from os.path import join as pjoin
 
-setup(name='circus',
+setup(name='spyking-circus',
       version='0.1',
       description='Fast spike sorting by template matching',
       url='http://www.yger.net/software/spyking-circus',
@@ -12,7 +12,7 @@ setup(name='circus',
       packages=['circus', 'circus.shared'],
       setup_requires=['cython', 'numpy', 'setuptools>0.18'],
       extras_require={'cuda' : ['cudamat']},
-      install_requires=['progressbar', 'mpi4py', 'mdp', 'hdf5storage', 'numpy', 'cython', 'scipy', 'matplotlib', 'configparser', 'h5py', 'termcolor'],
+      install_requires=['progressbar', 'mpi4py', 'mdp', 'hdf5storage', 'numpy', 'cython', 'scipy', 'matplotlib', 'h5py', 'termcolor'],
       dependency_links=['http://github.com/cudamat/cudamat/tarball/master#egg=cudamat'],
       scripts=[pjoin('bin', 'spyking-circus'),
                pjoin('bin', 'spyking-circus-subtask.py'),
