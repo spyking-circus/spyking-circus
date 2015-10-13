0518a367437b5e927b5ce72a4e2ebcc28be727c3
Pierre Yger <pierre.yger@gmail.com>
2015-10-13 16:50:53 +0200

    Overlaps are only created by node 0 if absent

--- circus/fitting.py
+++ circus/fitting.py
@@ -75,6 +75,11 @@ def main(filename, params, nb_cpu, nb_gpu, use_gpu):
     comm.Barrier()
 
     thresholds    = io.load_data(params, 'thresholds')
+    
+    if comm.rank == 0 and not os.path.exists(file_out_suff + '.overlap%s.hdf5' %extension):
+        c_overlap = io.get_overlaps(params)
+
+    comm.Barrier()
     c_overlap     = io.get_overlaps(params)
 
     if do_spatial_whitening or do_temporal_whitening:
