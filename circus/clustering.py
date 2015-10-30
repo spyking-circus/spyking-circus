0c45d50a73a7027daed71b37e02cb0d484c21e15
Pierre Yger <pierre.yger@gmail.com>
2015-10-30 09:10:00 +0100

    Default parameters

--- circus/clustering.py
+++ circus/clustering.py
@@ -32,7 +32,6 @@ def main(filename, params, nb_cpu, nb_gpu, use_gpu):
     make_plots     = params.getboolean('clustering', 'make_plots')
     sim_same_elec  = params.getfloat('clustering', 'sim_same_elec')
     cc_merge       = params.getfloat('clustering', 'cc_merge')
-    cc_delay       = int(params.getfloat('clustering', 'cc_delay')*sampling_rate*1e-3)
     noise_thr      = params.getfloat('clustering', 'noise_thr')
     smart_search   = numpy.ones(N_e, dtype=numpy.float32)*params.getfloat('clustering', 'smart_search')
     test_clusters  = params.getboolean('clustering', 'test_clusters')
