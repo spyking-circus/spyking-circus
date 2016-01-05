83c3f953de15906af171fd116a7ff8cc320b31bf
Pierre Yger <pierre.yger@gmail.com>
2016-01-05 08:09:15 +0100

    Less IO access

--- circus/shared/algorithms.py
+++ circus/shared/algorithms.py
@@ -3,7 +3,7 @@ matplotlib.use('Agg')
 import os
 os.environ['MDP_DISABLE_SKLEARN']='yes'
 import scipy.optimize, numpy, pylab, mdp, scipy.spatial.distance, scipy.stats, progressbar
-from circus.shared.files import load_data, write_datasets, get_overlaps
+from circus.shared.files import load_data, write_datasets, get_overlaps, get_nodes_and_edges
 
 def distancematrix(data, weight=None):
     
@@ -420,15 +420,15 @@ def delete_mixtures(comm, params, parallel_hdf5=False):
         sorted_temp    = numpy.argsort(norm_templates[:nb_temp])[::-1]
         M              = numpy.zeros((2, 2), dtype=numpy.float32)
         V              = numpy.zeros((2, 1), dtype=numpy.float32)
-        for count, k in enumerate(sorted_temp):
+        for count, k, bk in zip(numpy.arange(nb_temp), sorted_temp, best_elec):
 
             if not k in is_part_of_sum:
-                electrodes    = inv_nodes[edges[nodes[k]]]
+                electrodes    = inv_nodes[edges[nodes[bk]]]
                 overlap_k     = overlap[k]
                 is_in_area    = numpy.in1d(best_elec, electrodes)
                 for item in sorted_temp[:count]:
                     is_in_area[item] = False
-                all_idx       = numpy.arange(len(best_elec))[is_in_area]
+                all_idx       = numpy.arange(nb_temp)[is_in_area]
 
                 for i in all_idx:
                     overlap_i = overlap[i]
