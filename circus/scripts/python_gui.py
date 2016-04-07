781b3949756412234b0631e744a107d4da5afe9f
Pierre Yger <pierre.yger@gmail.com>
2016-04-07 10:25:25 +0200

    Ask before exporting. need to think about the best way to do it, as phy saves inplace...

--- circus/scripts/python_gui.py
+++ circus/scripts/python_gui.py
@@ -46,164 +46,177 @@ Syntax is circus-gui-python datafile [extension]
     data_offset    = params.getint('data', 'data_offset')
     probe          = read_probe(params)
     output_path    = params.get('data', 'file_out_suff') + '.GUI'
-    # if os.path.exists(output_path):
-    #     shutil.rmtree(output_path)
+    
+    do_export      = True
+
+    if os.path.exists(output_path):
+        key = ''
+        while key not in ['y', 'n']:
+            key = raw_input("Export already made! Do you want to erase everything? (y)es / (n)o")
+            if key =='y':
+                do_export = True
+            else:
+                do_export = False
+        if do_export:
+            shutil.rmtree(output_path)
+    
     N_e            = params.getint('data', 'N_e')
     N_t            = params.getint('data', 'N_t')
 
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
+    if do_export:
 
-    def generate_mapping(probe):
-        p         = {}
-        positions = []
-        nodes     = []
-        for key in probe['channel_groups'].keys():
-            p.update(probe['channel_groups'][key]['geometry'])
-            nodes     +=  probe['channel_groups'][key]['channels']
-            positions += [p[channel] for channel in probe['channel_groups'][key]['channels']]
-        idx       = numpy.argsort(nodes)
-        positions = numpy.array(positions)[idx]
-        return positions
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
 
-    def get_max_loc_channel(params):
-        nodes, edges    = circus.shared.utils.io.get_nodes_and_edges(params)
-        max_loc_channel = 0
-        for key in edges.keys():
-            if len(edges[key]) > max_loc_channel:
-                max_loc_channel = len(edges[key])
-        return max_loc_channel
+        def generate_mapping(probe):
+            p         = {}
+            positions = []
+            nodes     = []
+            for key in probe['channel_groups'].keys():
+                p.update(probe['channel_groups'][key]['geometry'])
+                nodes     +=  probe['channel_groups'][key]['channels']
+                positions += [p[channel] for channel in probe['channel_groups'][key]['channels']]
+            idx       = numpy.argsort(nodes)
+            positions = numpy.array(positions)[idx]
+            return positions
 
-    def write_results(path, params, extension):
-        result     = get_results(params, extension)
-        spikes     = numpy.zeros(0, dtype=numpy.int64)
-        clusters   = numpy.zeros(0, dtype=numpy.int32)
-        amplitudes = numpy.zeros(0, dtype=numpy.float32)
-        for key in result['spiketimes'].keys():
-            temp_id    = int(key.split('_')[-1])
-            data       = result['spiketimes'].pop(key)
-            spikes     = numpy.concatenate((spikes, data.astype(numpy.int64)))
-            amplitudes = numpy.concatenate((amplitudes, result['amplitudes'][key][:, 0]))
-            clusters   = numpy.concatenate((clusters, temp_id*numpy.ones(len(data), dtype=numpy.int32)))
-        
-        idx = numpy.argsort(spikes)
+        def get_max_loc_channel(params):
+            nodes, edges    = circus.shared.utils.io.get_nodes_and_edges(params)
+            max_loc_channel = 0
+            for key in edges.keys():
+                if len(edges[key]) > max_loc_channel:
+                    max_loc_channel = len(edges[key])
+            return max_loc_channel
 
-        numpy.save(os.path.join(output_path, 'spike_templates'), clusters[idx])
-        numpy.save(os.path.join(output_path, 'spike_times'), spikes[idx])
-        numpy.save(os.path.join(output_path, 'amplitudes'), amplitudes[idx])
-        return spikes[idx], clusters[idx]
+        def write_results(path, params, extension):
+            result     = get_results(params, extension)
+            spikes     = numpy.zeros(0, dtype=numpy.int64)
+            clusters   = numpy.zeros(0, dtype=numpy.int32)
+            amplitudes = numpy.zeros(0, dtype=numpy.float32)
+            for key in result['spiketimes'].keys():
+                temp_id    = int(key.split('_')[-1])
+                data       = result['spiketimes'].pop(key)
+                spikes     = numpy.concatenate((spikes, data.astype(numpy.int64)))
+                amplitudes = numpy.concatenate((amplitudes, result['amplitudes'][key][:, 0]))
+                clusters   = numpy.concatenate((clusters, temp_id*numpy.ones(len(data), dtype=numpy.int32)))
+            
+            idx = numpy.argsort(spikes)
 
-
-    def write_templates(path, params, extension):
-
-        max_loc_channel = get_max_loc_channel(params)
-        templates       = load_data(params, 'templates', extension)
-        N_tm            = templates.shape[1]//2
-        to_write        = numpy.zeros((N_tm, N_t, N_e), dtype=numpy.float32)
-        mapping         = numpy.zeros((N_tm, max_loc_channel), dtype=numpy.int32)
-
-        for t in xrange(N_tm):
-            tmp  = templates[:, t].toarray().reshape(N_e, N_t).T
-            x, y = tmp.nonzero()
-            to_write[t, x, y]                = tmp[x, y] 
-            nb_loc                           = len(numpy.unique(y))
-            mapping[t, numpy.arange(nb_loc)] = numpy.unique(y)
-
-        numpy.save(os.path.join(output_path, 'templates'), to_write)
-        numpy.save(os.path.join(output_path, 'templates_ind'), mapping)
-        return N_tm
+            numpy.save(os.path.join(output_path, 'spike_templates'), clusters[idx])
+            numpy.save(os.path.join(output_path, 'spike_times'), spikes[idx])
+            numpy.save(os.path.join(output_path, 'amplitudes'), amplitudes[idx])
+            return spikes[idx], clusters[idx]
 
 
-    def write_pcs(path, params, extension, spikes, labels, mode="a"):
+        def write_templates(path, params, extension):
 
-        max_loc_channel = get_max_loc_channel(params)
-        nb_features     = params.getint('whitening', 'output_dim')
-        nodes, edges    = get_nodes_and_edges(params)
-        N_total         = params.getint('data', 'N_total')
-        templates       = load_data(params, 'templates', extension)
-        N_tm            = templates.shape[1]//2
-        pc_features_ind = numpy.zeros((N_tm, max_loc_channel), dtype=numpy.int32)
-        clusters        = load_data(params, 'clusters', extension)
-        best_elec       = clusters['electrodes']
-        inv_nodes        = numpy.zeros(N_total, dtype=numpy.int32)
-        inv_nodes[nodes] = numpy.argsort(nodes)
+            max_loc_channel = get_max_loc_channel(params)
+            templates       = load_data(params, 'templates', extension)
+            N_tm            = templates.shape[1]//2
+            to_write        = numpy.zeros((N_tm, N_t, N_e), dtype=numpy.float32)
+            mapping         = numpy.zeros((N_tm, max_loc_channel), dtype=numpy.int32)
 
-        for count, elec in enumerate(best_elec):
-            nb_loc                = len(edges[nodes[elec]])
-            pc_features_ind[count, numpy.arange(nb_loc)] = inv_nodes[edges[nodes[elec]]]
+            for t in xrange(N_tm):
+                tmp  = templates[:, t].toarray().reshape(N_e, N_t).T
+                x, y = tmp.nonzero()
+                to_write[t, x, y]                = tmp[x, y] 
+                nb_loc                           = len(numpy.unique(y))
+                mapping[t, numpy.arange(nb_loc)] = numpy.unique(y)
 
-        basis_proj, basis_rec = load_data(params, 'basis')
+            numpy.save(os.path.join(output_path, 'templates'), to_write)
+            numpy.save(os.path.join(output_path, 'templates_ind'), mapping)
+            return N_tm
 
-        if mode == "a":
-            nb_pcs = len(spikes)
-        elif mode == "s":
-            nb_pcs = 0
+
+        def write_pcs(path, params, extension, spikes, labels, mode="a"):
+
+            max_loc_channel = get_max_loc_channel(params)
+            nb_features     = params.getint('whitening', 'output_dim')
+            nodes, edges    = get_nodes_and_edges(params)
+            N_total         = params.getint('data', 'N_total')
+            templates       = load_data(params, 'templates', extension)
+            N_tm            = templates.shape[1]//2
+            pc_features_ind = numpy.zeros((N_tm, max_loc_channel), dtype=numpy.int32)
+            clusters        = load_data(params, 'clusters', extension)
+            best_elec       = clusters['electrodes']
+            inv_nodes        = numpy.zeros(N_total, dtype=numpy.int32)
+            inv_nodes[nodes] = numpy.argsort(nodes)
+
+            for count, elec in enumerate(best_elec):
+                nb_loc                = len(edges[nodes[elec]])
+                pc_features_ind[count, numpy.arange(nb_loc)] = inv_nodes[edges[nodes[elec]]]
+
+            basis_proj, basis_rec = load_data(params, 'basis')
+
+            if mode == "a":
+                nb_pcs = len(spikes)
+            elif mode == "s":
+                nb_pcs = 0
+                for target in xrange(N_tm):
+                    nb_pcs += min(500, len(numpy.where(labels == target)[0]))
+
+            pc_features = numpy.zeros((nb_pcs, nb_features, max_loc_channel), dtype=numpy.float32)
+            count       = 0
+
+            pbar    = get_progressbar(N_tm)
+            all_idx = numpy.zeros(0, dtype=numpy.int32)
             for target in xrange(N_tm):
-                nb_pcs += min(500, len(numpy.where(labels == target)[0]))
+                if mode == "s":
+                    idx     = numpy.random.permutation(numpy.where(labels == target)[0])[:500]
+                    all_idx = numpy.concatenate((all_idx, idx))
+                elif mode == "a":
+                    idx  = numpy.where(labels == target)[0]
+                elec     = best_elec[target]
+                indices  = inv_nodes[edges[nodes[elec]]]
+                labels_i = target*numpy.ones(len(idx))
+                times_i  = numpy.take(spikes, idx)
+                sub_data = get_stas(params, times_i, labels_i, elec, neighs=indices, nodes=nodes)
+                pcs      = numpy.dot(sub_data, basis_proj)
+                pcs      = numpy.swapaxes(pcs, 1,2)
+                if mode == "s":
+                    pc_features[count:count+len(idx), :, :len(indices)] = pcs                    
+                    count += len(idx)
+                else:
+                    pc_features[idx, :, :len(indices)] = pcs
 
-        pc_features = numpy.zeros((nb_pcs, nb_features, max_loc_channel), dtype=numpy.float32)
-        count       = 0
-
-        pbar    = get_progressbar(N_tm)
-        all_idx = numpy.zeros(0, dtype=numpy.int32)
-        for target in xrange(N_tm):
+                pbar.update(target)
+            pbar.finish()
+            
+            numpy.save(os.path.join(output_path, 'pc_features'), pc_features) # nspikes, nfeat, n_loc_chan
+            numpy.save(os.path.join(output_path, 'pc_feature_ind'), pc_features_ind) #n_templates, n_loc_chan
             if mode == "s":
-                idx     = numpy.random.permutation(numpy.where(labels == target)[0])[:500]
-                all_idx = numpy.concatenate((all_idx, idx))
-            elif mode == "a":
-                idx  = numpy.where(labels == target)[0]
-            elec     = best_elec[target]
-            indices  = inv_nodes[edges[nodes[elec]]]
-            labels_i = target*numpy.ones(len(idx))
-            times_i  = numpy.take(spikes, idx)
-            sub_data = get_stas(params, times_i, labels_i, elec, neighs=indices, nodes=nodes)
-            pcs      = numpy.dot(sub_data, basis_proj)
-            pcs      = numpy.swapaxes(pcs, 1,2)
-            if mode == "s":
-                pc_features[count:count+len(idx), :, :len(indices)] = pcs                    
-                count += len(idx)
-            else:
-                pc_features[idx, :, :len(indices)] = pcs
+                numpy.save(os.path.join(output_path, 'pc_feature_spike_ids'), all_idx)
 
-            pbar.update(target)
-        pbar.finish()
+        print_and_log(["Exporting data for the phy GUI..."], 'info', params)
         
-        numpy.save(os.path.join(output_path, 'pc_features'), pc_features) # nspikes, nfeat, n_loc_chan
-        numpy.save(os.path.join(output_path, 'pc_feature_ind'), pc_features_ind) #n_templates, n_loc_chan
-        if mode == "s":
-            numpy.save(os.path.join(output_path, 'pc_feature_spike_ids'), all_idx)
+        numpy.save(os.path.join(output_path, 'whitening_mat'), load_data(params, 'spatial_whitening'))
+        numpy.save(os.path.join(output_path, 'channel_positions'), generate_mapping(probe))
+        nodes, edges   = get_nodes_and_edges(params)
+        numpy.save(os.path.join(output_path, 'channel_map'), nodes)
 
-    print_and_log(["Exporting data for the phy GUI..."], 'info', params)
-    
-    numpy.save(os.path.join(output_path, 'whitening_mat'), load_data(params, 'spatial_whitening'))
-    numpy.save(os.path.join(output_path, 'channel_positions'), generate_mapping(probe))
-    nodes, edges   = get_nodes_and_edges(params)
-    numpy.save(os.path.join(output_path, 'channel_map'), nodes)
-
-    spikes, clusters = write_results(output_path, params, extension)    
-    N_tm = write_templates(output_path, params, extension)
-    similarities = h5py.File(file_out_suff + '.templates%s.hdf5' %extension, 'r+', libver='latest').get('maxoverlap')
-    norm = params.getint('data', 'N_e')*params.getint('data', 'N_t')
-    numpy.save(os.path.join(output_path, 'similar_templates'), similarities[:N_tm, :N_tm]/norm)
+        spikes, clusters = write_results(output_path, params, extension)    
+        N_tm = write_templates(output_path, params, extension)
+        similarities = h5py.File(file_out_suff + '.templates%s.hdf5' %extension, 'r+', libver='latest').get('maxoverlap')
+        norm = params.getint('data', 'N_e')*params.getint('data', 'N_t')
+        numpy.save(os.path.join(output_path, 'similar_templates'), similarities[:N_tm, :N_tm]/norm)
 
 
-    key = ''
-    while key not in ['a', 's', 'n']:
-        key = raw_input("Do you want SpyKING CIRCUS to export PCs? (a)ll / (s)ome / (n)o ")
-    
-    if key in ['a', 's']:
-        if not os.path.exists(os.path.join(output_path, 'pc_features.npy')):
-            write_pcs(output_path, params, extension, spikes, clusters, key)
-    elif key in ['n']:
-        if os.path.exists(os.path.join(output_path, 'pc_features.npy')):
-            os.remove(os.path.join(output_path, 'pc_features.npy'))
-        if os.path.exists(os.path.join(output_path, 'pc_feature_ind.npy')):
-            os.remove(os.path.join(output_path, 'pc_feature_ind.npy'))
+        key = ''
+        while key not in ['a', 's', 'n']:
+            key = raw_input("Do you want SpyKING CIRCUS to export PCs? (a)ll / (s)ome / (n)o ")
+        
+        if key in ['a', 's']:
+            if not os.path.exists(os.path.join(output_path, 'pc_features.npy')):
+                write_pcs(output_path, params, extension, spikes, clusters, key)
+        elif key in ['n']:
+            if os.path.exists(os.path.join(output_path, 'pc_features.npy')):
+                os.remove(os.path.join(output_path, 'pc_features.npy'))
+            if os.path.exists(os.path.join(output_path, 'pc_feature_ind.npy')):
+                os.remove(os.path.join(output_path, 'pc_feature_ind.npy'))
 
     print_and_log(["Launching the phy GUI..."], 'info', params)
 
-    gui_params         = {}
-
+    gui_params                   = {}
     gui_params['dat_path']       = params.get('data', 'data_file')
     gui_params['n_channels_dat'] = params.getint('data', 'N_total')
     gui_params['n_features_per_channel'] = 5
