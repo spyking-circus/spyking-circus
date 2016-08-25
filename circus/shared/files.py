9fc4a013c08426ce1ca8222f7651e1dd2d5eb452
yger <pierre.yger@gmail.com>
2016-08-25 13:48:55 +0200

    Avoid some duplications of spike times during the fitting procedure

--- circus/shared/files.py
+++ circus/shared/files.py
@@ -210,7 +210,7 @@ def load_parameters(file_name):
         parser.set('data', 'radius', str(int(probe['radius'])))
 
     new_values = [['fitting', 'amp_auto', 'bool', 'True'], 
-                  ['fitting', 'refractory', 'float', '0'],
+                  ['fitting', 'refractory', 'float', '0.5'],
                   ['fitting', 'collect_all', 'bool', 'False'],
                   ['data', 'global_tmp', 'bool', 'True'],
                   ['data', 'chunk_size', 'int', '10'],
