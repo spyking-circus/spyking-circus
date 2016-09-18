9ec635750c7009d44f7ca1cf12ce82debb960305
yger <pierre.yger@gmail.com>
2016-09-18 18:56:04 +0200

    WIP on openephys support

--- circus/files/openephys.py
+++ circus/files/openephys.py
@@ -10,14 +10,23 @@ class OpenEphysFile(DataFile):
     _parallel_write = True
 
     # constants
-    NUM_HEADER_BYTES = 1024L
-    SAMPLES_PER_RECORD = 1024L
-    RECORD_SIZE = 8 + 16 + SAMPLES_PER_RECORD*2 + 10 # size of each continuous record in bytes
-    RECORD_MARKER = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 255])
+    NUM_HEADER_BYTES   = 1024
+    SAMPLES_PER_RECORD = 1024
+    RECORD_SIZE        = 8 + 16 + SAMPLES_PER_RECORD*2 + 10 # size of each continuous record in bytes
 
     def _get_sorted_channels(self, folderpath):
         return sorted([int(f.split('_CH')[1].split('.')[0]) for f in os.listdir(folderpath) 
                     if '.continuous' in f and '_CH' in f]) 
+
+    def _read_header(self, file):
+        header = { }
+        f = open(file, 'r')
+        h = f.read(1024).replace('\n','').replace('header.','')
+        for i,item in enumerate(h.split(';')):
+            if '=' in item:
+                header[item.split(' = ')[0]] = item.split(' = ')[1]
+        f.close()
+        return header
 
     def __init__(self, file_name, params, empty=False, comm=None):
 
@@ -25,21 +34,27 @@ class OpenEphysFile(DataFile):
         folder_path = os.path.dirname(os.path.realpath(self.file_name))
         self.all_channels = self._get_sorted_channels(folder_path)
         self.all_files = [os.path.join(folder_path, '100_CH'+x+'.continuous') for x in map(str,self.all_channels)]
+        self.header = self._read_header(self.all_files[0])
+        self.rate   = float(self.header['sampleRate'])
+        self.data_dtype  = self.params.get('data', 'data_dtype')
+
+        if self.data_dtype not in ['int16', 'float32']:
+            print_error(['Data type for OpenEphys sould be int16 or float32'])
+            sys.exit(0)
         
+        self.data_offset = self.NUM_HEADER_BYTES
+        self.set_dtype_offset(self.data_dtype)
         if not self.empty:
             self._get_info_()
 
     def _get_info_(self):
 
-        self.empty = False
+        self.empty       = False
         self.open()
-        #self.data_dtype  = self.my_file.get(self.h5_key + '0').dtype
-        #self.compression = self.my_file.get(self.h5_key + '0').compression
-        #self.rate        = self.my_file.get(self.h5_key + 'rate')[0]
-
-        #self.size = self.my_file.get(self.h5_key + '/0').shape[0]
-        self.set_dtype_offset(self.data_dtype)
-        self.N_tot       = len(self.my_file.get(self.h5_key))
+        g = open(self.all_files[0], 'rb')
+        self.size        = (os.fstat(g.fileno()).st_size//self.RECORD_SIZE) * self.SAMPLES_PER_RECORD
+        g.close()
+        self.N_tot       = len(self.all_files)
         self._shape      = (self.size, self.N_tot)
         self.max_offset  = self._shape[0]
         self.data_offset = 0
@@ -47,6 +62,15 @@ class OpenEphysFile(DataFile):
 
     def allocate(self, shape, data_dtype=None):
         pass
+
+    def copy_header(self, file_out):
+        if self.is_master:
+            fin  = open(self.file_name, 'rb')
+            fout = open(file_out, 'wb')
+            data = fin.read(self.data_offset)
+            fout.write(data)
+            fin.close()
+            fout.close()
 
     def get_data(self, idx, chunk_size=None, padding=(0, 0), nodes=None):
         
@@ -64,10 +88,14 @@ class OpenEphysFile(DataFile):
         for i in nodes:
             local_chunk[:, i] = self.data[i][idx*numpy.int64(chunk_size)+padding[0]:(idx+1)*numpy.int64(chunk_size)+padding[1]]
         
+        if self.data_dtype != 'int16':
+            local_chunk *= self.header['bitVolts']
+
         local_chunk  = local_chunk.astype(numpy.float32)
         local_chunk -= self.dtype_offset
 
         return numpy.ascontiguousarray(local_chunk)
+
 
     def set_data(self, time, data):
         
@@ -77,7 +105,7 @@ class OpenEphysFile(DataFile):
             self.data[i][time:time+data.shape[0]] = data[:, i]
 
     def open(self, mode='r'):
-        self.data = [numpy.memmap(self.all_files[i], offset=self.data_offset, dtype=self.data_dtype, mode=mode) for i in xrange(self.N_tot)]
+        self.data = [numpy.memmap(self.all_files[i], offset=self.data_offset, dtype=numpy.int16, mode=mode) for i in xrange(self.N_tot)]
         
     def close(self):
         del self.data
