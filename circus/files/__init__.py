d3d6f8a4e4d684f27190fe5f2d8825f7a3207af4
yger <pierre.yger@gmail.com>
2016-09-19 16:01:06 +0200

    Removing old kwik file, now named kwd

--- circus/files/__init__.py
+++ circus/files/__init__.py
@@ -3,7 +3,6 @@ from datafile import *
 from raw_binary import RawBinaryFile
 from mcs_raw_binary import RawMCSFile
 from hdf5 import H5File
-from kwik import KwikFile
 from kwd import KwdFile
 from openephys import OpenEphysFile
 
@@ -24,7 +23,6 @@ __supported_data_files__ = {
 	RawBinaryFile._description : RawBinaryFile,
 	RawMCSFile._description : RawMCSFile,
 	H5File._description : H5File,
-	KwikFile._description : KwikFile,
 	OpenEphysFile._description : OpenEphysFile,
 	KwdFile._description : KwdFile
 }
