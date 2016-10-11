4620a6fc3bb6f2b0883d7e47c0d9091b17a37cd7
yger <pierre.yger@gmail.com>
2016-10-11 08:38:19 +0200

    WIP streams

--- circus/files/stream.py
+++ circus/files/stream.py
@@ -9,30 +9,29 @@ class Stream(object):
     def __init__(self, data_sources, **kwargs):
 
         self.sources = data_sources
-
-
+        self._times  = [0]
+        for source in self.sources:
+            self._times += [source.duration]
+        
     def get_data(self, idx, chunk_size, padding=(0, 0), nodes=None):
         
         if not hasattr(self, '_chunks_in_sources'):
             print_and_log(['The Stream must be initialized with the analyze() function'], 'error', logger)
 
         cidx = numpy.searchsorted(idx, self._chunks_in_sources)
-
-        return self.sources[cidx].get_data(idx - 0, chunk_size, padding, nodes)
+        return self.sources[cidx].get_data(idx - self._chunks_in_sources[cidx], chunk_size, padding, nodes)
 
         
     def get_snippet(self, time, length, nodes=None):
-        return self.get_data(0, chunk_size=length, padding=(time, time), nodes=nodes)
+        cidx  = numpy.searchsorted(time, numpy.cumsum(self._times))
+        time -= numpy.cumsum(self._times)[cidx]
+        return self.sources[cidx].get_data(0, chunk_size=length, padding=(time, time), nodes=nodes)
 
 
     def set_data(self, time, data):
-        
-        if not hasattr(self, '_chunks_in_sources'):
-            print_and_log(['The Stream must be initialized with the analyze() function'], 'error', logger)
+        cidx = numpy.searchsorted(time, numpy.cumsum(self._times))
+        return self.sources[cidx].set_data(time - numpy.cumsum(self._times)[cidx], data)
 
-        cidx = numpy.searchsorted(idx, self._chunks_in_sources)
-        
-        return self.sources[idx].set_data()
 
     def analyze(self, chunk_size):
         nb_chunks = 0
@@ -43,18 +42,18 @@ class Stream(object):
             if b > 0:
                 nb_chunks += 1
             self._chunks_in_sources += [nb_chunks]
-
         return nb_chunks
+
 
     @property
     def duration(self):
-        for source in self.sources:
-            duration += b.duration
-        return duration
+        return numpy.sum(self._times)
+
 
     @property
     def shape(self):
         return (self.duration, self.nb_channels)
+
          
     @property
     def nb_channels(self):
