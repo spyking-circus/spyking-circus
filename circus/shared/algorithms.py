7e9860a1675dc03442da55d4384d224980fcfc13
yger <pierre.yger@gmail.com>
2017-03-02 08:42:52 +0100

    weight errors by density

--- circus/shared/algorithms.py
+++ circus/shared/algorithms.py
@@ -35,9 +35,9 @@ def fit_rho_delta(xdata, ydata, smart_select=False, display=False, max_clusters=
         try:
             result, pcov = scipy.optimize.curve_fit(myfunc, xdata, ydata, p0=[a_0, 1., 1.])
             prediction   = myfunc(xdata, result[0], result[1], result[2])
-            difference   = ydata - prediction
+            difference   = rho*(ydata - prediction)
             z_score      = (difference - difference.mean())/difference.std()
-            subidx       = numpy.where((z_score >= 1.) & (rho >= rho.mean()))[0]
+            subidx       = numpy.where((z_score >= 1.))[0]
         except Exception:
             subidx = numpy.argsort(xdata*numpy.log(1 + ydata))[::-1][:max_clusters]
         
