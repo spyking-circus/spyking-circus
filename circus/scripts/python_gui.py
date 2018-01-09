f344156520577cfe68f8f74f8e6c0942d55dd95f
yger <pierre.yger@gmail.com>
2018-01-09 10:46:05 +0100

    Fix for datafiles without data_offset

--- circus/scripts/python_gui.py
+++ circus/scripts/python_gui.py
@@ -64,7 +64,7 @@ def main(argv=None):
 
     data_file      = params.get_data_file()
     data_dtype     = data_file.data_dtype
-    if hasattr(data_file, 'data_offset'):
+    if data_file.params.has_key('data_offset'):
         data_offset = data_file.data_offset
     else:
         data_offset = 0
