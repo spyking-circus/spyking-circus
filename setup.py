47b2778d052620818865f890ac284ef80f4bcb12
yger <pierre.yger@gmail.com>
2019-09-06 15:34:25 +0200

    sip in included

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os.path import join as pjoin
 import sys, subprocess, re
 
 requires = ['mpi4py', 'numpy', 'cython', 'scipy', 'matplotlib', 'h5py', 'colorama',
-            'psutil', 'tqdm', 'blosc', 'statsmodels', 'scikit-learn', 'sip']
+            'psutil', 'tqdm', 'blosc', 'statsmodels', 'scikit-learn']
 
 from setuptools import setup
 
