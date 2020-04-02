8ef01a50750bd590f2ce132d856b117fd1b0c486
yger <pierre.yger@gmail.com>
2020-04-02 09:34:31 +0200

    WIP

--- circus/shared/parser.py
+++ circus/shared/parser.py
@@ -207,7 +207,7 @@ class CircusParser(object):
                         ['clustering', 'fine_amplitude', 'bool', 'True'],
                         ['detection', 'jitter_range', 'float', '0.2'],
                         ['detection', 'smoothing_factor', 'float', '1.48'],
-                        ['detection', 'rejection_threshold', 'float', '5'],
+                        ['detection', 'rejection_threshold', 'float', '2'],
                         ['data', 'memory_usage', 'float', '0.1'],
                         ['clustering', 'safety_time', 'string', 'auto'],
                         ['clustering', 'savgol', 'bool', 'True'],
