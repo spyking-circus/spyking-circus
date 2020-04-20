e07369193434bc010fee4c5775bfb43b9a3d640a
yger <pierre.yger@gmail.com>
2020-04-20 13:24:32 +0200

    Fix

--- circus/filtering.py
+++ circus/filtering.py
@@ -169,9 +169,12 @@ def main(params, nb_cpu, nb_gpu, use_gpu):
             local_chunk, t_offset =  data_file_in.get_data(gidx, chunk_size, padding)
 
             if do_filtering:
-                local_chunk -= numpy.median(local_chunk, 0)
                 local_chunk = signal.filtfilt(b, a, local_chunk, axis=0)
-                local_chunk = local_chunk[numpy.abs(padding[0]):-numpy.abs(padding[1])]
+                local_chunk -= numpy.median(local_chunk, 0)
+                if not is_last:
+                    local_chunk = local_chunk[numpy.abs(padding[0]):-numpy.abs(padding[1])]
+                else:
+                    local_chunk = local_chunk[numpy.abs(padding[0]):]
             else:
                 local_chunk = local_chunk[numpy.abs(padding[0]):-numpy.abs(padding[1])]
 
