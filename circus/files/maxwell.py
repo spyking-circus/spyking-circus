355495ab3ac0ccd71edf3e7b592a2902e72bac22
yger <pierre.yger@gmail.com>
2020-08-31 16:31:14 +0200

    Maxwell wrapper

--- circus/files/maxwell.py
+++ circus/files/maxwell.py
@@ -2,9 +2,7 @@ import h5py, numpy, re, sys, logging, os
 from .hdf5 import H5File
 from circus.shared.messages import print_and_log
 
-
 logger = logging.getLogger(__name__)
-
 
 class MaxwellFile(H5File):
 
@@ -13,58 +11,45 @@ class MaxwellFile(H5File):
     parallel_write = h5py.get_config().mpi
 
     _required_fields = {}
-    _default_values = {}
-
-    def _generate_probe(self, mapping, nb_channels):
-        dirname = os.path.dirname(os.path.abspath(self.file_name))
-        probe_name = os.path.join(dirname, 'mapping.prb')
-        channels = list(mapping['channel'])
-        x_pos = list(mapping['x'])
-        y_pos = list(mapping['y'])
-        geometry = {}
-        for c, x, y in zip(channels, x_pos, y_pos):
-            geometry[c] = (x, y)
-        f=open(probe_name, 'w')
-        to_write = '''
-total_nb_channels=%s
-radius = 100
-channel_groups = {
-1: {
-    'channels': %s,
-    'geometry': {
-    %s
-    }
-    }
-}
-''' %(nb_channels, channels, geometry)
-        f.write(to_write)
-        f.close()
+    _default_values = {'h5_key': ''}
 
     def _read_from_header(self):
 
         header = {}
 
-        self.params['h5_key'] = 'sig'
+        my_file = h5py.File(self.file_name, mode='r')
+        header['version'] = my_file['version'][0]
 
-        self._open() 
-        f = h5py.File(self.file_name, mode='r')
-        header['h5_key'] = self.h5_key
-        header['sampling_rate'] = 20000
-
-        header['gain'] = 1.0
-        header['dtype_offset'] = 'auto'
-        header['data_dtype'] = self.my_file.get(header['h5_key']).dtype
-        self.compression = self.my_file.get(header['h5_key']).compression
+        if header['version'] == b'20160704':
+            h5_key = 'sig'
+            header['h5_key'] = 'sig'
+            header['sampling_rate'] = 20000
+            header['dtype_offset'] = 'auto'
+        else:
+            pass
+            header['sampling_rate'] = 10000
+            header['dtype_offset'] = 512
+        
+        header['gain'] = my_file.get('settings/lsb')[0]
+        header['data_dtype'] = my_file.get(header['h5_key']).dtype
+        self.compression = my_file.get(header['h5_key']).compression
 
         self._check_compression()
 
-        nb_channels, n_frames = self.my_file.get(header['h5_key']).shape
+        nb_channels, n_frames = my_file.get(header['h5_key']).shape
         self.size = nb_channels * n_frames
         header['nb_channels'] = nb_channels
         self._shape = (n_frames, header['nb_channels'])
-        mapping = f['mapping'][:]
-        self._generate_probe(mapping, nb_channels)
-
-        self._close()
-
+        my_file.close()
         return header
+
+    def _read_chunk(self, do_slice, t_start, t_stop, nodes):
+        if do_slice:
+            local_chunk = self.data[nodes, t_start:t_stop].T
+        else:
+            local_chunk = self.data[:, t_start:t_stop].T
+        return local_chunk
+
+    def write_chunk(self, time, data):
+        data = self._unscale_data_from_float32(data)
+        self.data[:, time:time+data.shape[0]] = data.T
